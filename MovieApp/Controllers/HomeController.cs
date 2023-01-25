d4e58a4088a5ca57655529f1556fa49b7eb80fd2
Andrew Grant McCoy <andrewsmccoy@outlook.com>
2023-01-25 01:29:05 -0600

    initial Commit

---
+++ MovieApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Models;
+using MovieList.Models;
+using System.Diagnostics;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre)
+                .OrderBy(m => m.Name).ToList(); return View(movies);
+        }
+    }
+}
